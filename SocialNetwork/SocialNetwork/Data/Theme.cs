c76098f69463dd369365596a2efe3767dd7c2e82
Catherine <Catherine25@users.noreply.github.com>
2019-10-06 21:09:28 +0300

    Add Theme.cs

---
+++ SocialNetwork/SocialNetwork/Data/Theme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using Xamarin.Forms;
+
+namespace SocialNetwork.Data
+{
+    public class Theme
+    {
+        public Theme(string name, Color[] colors)
+        {
+            Name = name;
+            this.colors = colors;
+        }
+
+        public string Name;
+        public Color[] colors;
+    }
+}
