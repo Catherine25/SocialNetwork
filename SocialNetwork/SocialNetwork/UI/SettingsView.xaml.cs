3394eacf464c01945966b8f155e0f7fcbbe9d730
Catherine <Catherine25@users.noreply.github.com>
2019-10-06 21:21:47 +0300

    Add: - themes - ThemePicker_SelectedIndexChanged() Modify SettingsView

--- SocialNetwork/SocialNetwork/UI/SettingsView.xaml.cs
+++ SocialNetwork/SocialNetwork/UI/SettingsView.xaml.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+//TODO: Add themes
 
 namespace SocialNetwork.UI
 {
@@ -14,11 +15,32 @@ namespace SocialNetwork.UI
     public partial class SettingsView : ContentView
     {
         User user;
+        Dictionary<string, Theme> themes = new Dictionary<string, Theme>
+        {
+            {
+                "Theme1", new Theme("default", new Color[] {
+                Color.FromRgb(26,24,24),
+                Color.Black,
+                Color.Black,
+                Color.Black })
+            }
+        };
+        
         public SettingsView(User user)
         {
             InitializeComponent();
 
             this.user = user;
+
+            themePicker.SelectedIndexChanged += ThemePicker_SelectedIndexChanged;
+        }
+
+        private void ThemePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string item = (string)themePicker.SelectedItem;
+            Theme theme = themes[item];
+            color1.BackgroundColor = theme.colors[0];
+            user.Theme = theme;
         }
     }
 }
