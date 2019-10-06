18ca0c25679575cafb227de9f0feb8ee8037b3a8
Catherine <Catherine25@users.noreply.github.com>
2019-10-06 21:13:00 +0300

    Add: - Avatar null check - Theme;

--- SocialNetwork/SocialNetwork/Data/User.cs
+++ SocialNetwork/SocialNetwork/Data/User.cs
@@ -13,12 +13,23 @@ namespace SocialNetwork.Data
             Name = name;
             Bio = bio;
 
+            if (Avatar == null)
+                Avatar = new Image();
+
             Friends = new List<User>();
+
+            Theme = new Theme(
+                "default", new Color[] {
+                Color.FromRgb(26,24,24),
+                Color.FromRgb(85,89,95),
+                Color.Black,
+                Color.Black });
         }
 
         public Image Avatar;
         public string Name;
         public string Bio;
+        public Theme Theme;
 
         public List<User> Friends;
         public List<Group> Groups;
