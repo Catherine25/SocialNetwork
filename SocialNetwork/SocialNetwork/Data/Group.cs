9a13e90f2401c8d08ef832abd2b6f0ab0765c07e
Catherine <Catherine25@users.noreply.github.com>
2019-10-16 10:16:40 +0300

    Add imageLink in Group constructor; Add AvatarLink in Group class;

--- SocialNetwork/SocialNetwork/Data/Group.cs
+++ SocialNetwork/SocialNetwork/Data/Group.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SocialNetwork.Data
 {
     public class Group
     {
-        public Group(string newTitle, string newDescription)
+        public Group(string newTitle, string newDescription, string imageLink)
         {
+            AvatarLink = imageLink;
             Title = newTitle;
             Description = newDescription;
+
+            if (AvatarLink == null)
+                AvatarLink = "";
         }
 
+        public string AvatarLink;
         public string Title;
         public string Description;
         public List<User> members;
