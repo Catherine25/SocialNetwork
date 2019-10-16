5b1b41a5b0bc4b00dcfda7f2566c773afd306587
Catherine <Catherine25@users.noreply.github.com>
2019-10-16 11:14:22 +0300

    Implement IColorable; Change: - UserView(); - buttons' texts; Add Image_Clicked();

--- SocialNetwork/SocialNetwork/UI/UserView.xaml.cs
+++ SocialNetwork/SocialNetwork/UI/UserView.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using SocialNetwork.Data;
+using SocialNetwork.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +15,7 @@ using Xamarin.Forms.Xaml;
 namespace SocialNetwork.UI
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class UserView : ContentView
+    public partial class UserView : ContentView, IColorable
     {
         User CurrentUser;
         User Visitor;
@@ -26,22 +27,42 @@ namespace SocialNetwork.UI
             CurrentUser = user;
             Visitor = visitor;
 
+            SetTheme(user.Theme);
+
             if (visitor == user)
                 removeBt.IsVisible = false;
             else
             {
                 removeBt.Clicked += RemoveBt_Clicked;
-                removeBt.Text = visitor.Friends.Contains(CurrentUser) ? "Remove from friends" : "Add as friend";
+                removeBt.Text = visitor.Friends.Contains(CurrentUser) ? "Remove from friends list" : "Add to friends list";
             }
 
-            image = user.Avatar;
+            if (user.AvatarLink != "")
+                image.Source = ImageSource.FromUri(new Uri(user.AvatarLink));
+            else
+                image.Source = "https://www.indiannaturaloils.com/categories-images/no-photo.jpg";
+
             name.Text = user.Name;
             bio.Text = user.Bio;
 
-            image.BackgroundColor = user.Theme.colors[0];
-            name.TextColor = user.Theme.colors[0];
-            bio.TextColor = user.Theme.colors[0];
+            image.Clicked += Image_Clicked;
         }
+
+        private async void Image_Clicked(object sender, EventArgs e)
+        {
+            string uriString = await Clipboard.GetTextAsync();
+
+            try
+            {
+                image.Source = ImageSource.FromUri(new Uri(uriString));
+            }
+            catch
+            {
+                image.Source = "https://www.indiannaturaloils.com/categories-images/no-photo.jpg";
+            }
+        }
+
+        public void SetTheme(Theme theme) => (this as View).SetTheme(theme);
 
         private void RemoveBt_Clicked(object sender, EventArgs e)
         {
@@ -49,13 +70,13 @@ namespace SocialNetwork.UI
 
             if(Visitor.Friends.Contains(CurrentUser))
             {
-                button.Text = "Add as friend";
+                button.Text = "Add to friends list";
                 Visitor.Friends.Remove(CurrentUser);
             }
             else
             {
                 Visitor.Friends.Add(CurrentUser);
-                button.Text = "Remove from friends";
+                button.Text = "Remove from friends list";
             }
         }
     }
