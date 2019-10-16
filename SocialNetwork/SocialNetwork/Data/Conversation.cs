c78ecd8b9e61967157ecedc8d8d169395b458207
Catherine <Catherine25@users.noreply.github.com>
2019-10-16 10:15:17 +0300

    Set class public Simplified validation

--- SocialNetwork/SocialNetwork/Data/Conversation.cs
+++ SocialNetwork/SocialNetwork/Data/Conversation.cs
@@ -4,19 +4,16 @@ using System.Text;
 
 namespace SocialNetwork.Data
 {
-    class Conversation
+    public class Conversation
     {
         public Conversation(User user1, User user2, List<Message> newMessages)
         {
             member1 = user1;
             member2 = user2;
 
-            if (newMessages != null)
-                if (newMessages.Count != 0)
-                    messages = newMessages;
-                else throw new Exception();
-            else throw new Exception();
+            messages = newMessages ?? throw new Exception();
         }
+
         public readonly User member1;
         public readonly User member2;
         public readonly List<Message> messages;
