e55a234d9ed7f7d845594405a5fc2fc747fe4466
Catherine <Catherine25@users.noreply.github.com>
2019-10-16 10:48:54 +0300

    Add: - standartCount; - GenerateAvatarLink(); - GenerateName(); - GenerateSentence(); - GenerateMessage(); - GenerateDateTime(); - Avatars[]; Modify: - GenerateUser(); - GenerateGroup(); - GenerateConversation(); - GenerateMessages(); - GenerateConversations();

--- SocialNetwork/SocialNetwork/Services/Test.cs
+++ SocialNetwork/SocialNetwork/Services/Test.cs
@@ -9,56 +9,82 @@ namespace SocialNetwork.Services
 {
     class Test
     {
+        private const int standartCount = 10; 
+        private static string GenerateAvatarLink() => Avatars[Random.Next(standartCount)];
+        private static string GenerateName() => Names[Random.Next(standartCount)];
+        private static string GenerateSentence() => Sentences[Random.Next(standartCount)];
+
         public static User GenerateUser() =>
-            new User(null, Names[Random.Next(0, 10)], Sentences[Random.Next(0, 10)]);
+            new User(GenerateAvatarLink(), GenerateName(), GenerateSentence());
 
         public static Group GenerateGroup() =>
-            new Group(Sentences[Random.Next(0, 10)], Sentences[Random.Next(0, 10)]);
+            new Group(GenerateSentence(), GenerateSentence(), GenerateAvatarLink());
+
+        public static Message GenerateMessage() =>
+            new Message(GenerateSentence(), GenerateDateTime(), Random.Next(2) == 0 );
 
         public static Conversation GenerateConversation(User user) 
         {
+            Debug.WriteLine("GenerateConversation() running");
+
             List<Message> generatedMessages = new List<Message>(GenerateMessages(user));
-            Debug.Assert(generatedMessages.Count != 0);
-            IOrderedEnumerable<Message> sorted = from Message m in generatedMessages
-                                                 orderby m.DateTime
-                                                 select m;
+            List<Message> messages = generatedMessages.OrderBy(X => X.DateTime).ToList();
 
-            List<Message> messages = new List<Message>();
-            
-            foreach (Message m in sorted)
-                messages.Add(m);
-
-            Conversation c = new Conversation(
-                user,
-                GenerateUser(),
-                messages);
-
+            Conversation c = new Conversation(user, GenerateUser(), messages);
             return c;
         }
 
-        public static IEnumerable<Message> GenerateMessages(User user) {
+        private static DateTime GenerateDateTime()
+        {
+            //Debug.WriteLine("GenerateDateTime() running");
+
+            DateTime dateTime = new DateTime(1999, 1, 25);
+            TimeSpan range = (DateTime.Now - dateTime);
+            //Debug.WriteLine("range is " + range.ToString());
+            
+            int daysRange = range.Days;
+            int hoursRange = range.Hours;
+            int minutesRange = range.Minutes;
+            int secondsRange = range.Seconds;
+
+            dateTime = dateTime.AddDays(Random.Next(daysRange));
+            dateTime = dateTime.AddHours(Random.Next(hoursRange));
+            dateTime = dateTime.AddMinutes(Random.Next(minutesRange));
+            dateTime = dateTime.AddSeconds(Random.Next(secondsRange));
+
+            //Debug.WriteLine("Final dateTime is: " + dateTime);
+            return dateTime;
+        }
+
+        public static IEnumerable<Message> GenerateMessages(User user)
+        {
+
+            Debug.WriteLine("GenerateMessages() running");
 
             IList<Message> messages = new List<Message>();
 
-            int count = Random.Next(1, 10);
+            int count = Random.Next(1, standartCount);
+
             for (int i = 0; i < count; i++)
             {
-                messages.Add(new Message
-                {
-                    DateTime = DateTime.Today,
-                    Text = Sentences[Random.Next(0, 10)],
-                    isFromMember1 = Random.Next(0, 1) == 0
-                });
+                Message message = GenerateMessage();
+
+                if (messages.Any(X => X.DateTime.ToString() == message.DateTime.ToString()))
+                    continue;
+                else
+                    messages.Add(message);
             }
 
-            Debug.WriteLine("Generated " + count + " messages");
+            Debug.WriteLine("Generated " + messages.Count + " messages");
 
             return messages;
         }
 
         public static IEnumerable<Conversation> GenerateConversations(User user) {
-            
-            int length = Random.Next(0, 10);
+
+            Debug.WriteLine("GenerateConversations() running");
+
+            int length = Random.Next(standartCount);
             IList<Conversation> conversations = new List<Conversation>();
 
             for (int i = 0; i < length; i++)
@@ -71,7 +97,7 @@ namespace SocialNetwork.Services
         {
             List<User> friends = new List<User>();
 
-            int length = Random.Next(0, 10);
+            int length = Random.Next(standartCount);
 
             for (int i = 0; i < length; i++)
                 friends.Add(GenerateUser());
@@ -83,7 +109,7 @@ namespace SocialNetwork.Services
         {
             List<Group> groups = new List<Group>();
 
-            int length = Random.Next(0, 10);
+            int length = Random.Next(standartCount);
 
             for (int i = 0; i < length; i++)
                 groups.Add(GenerateGroup());
@@ -93,7 +119,8 @@ namespace SocialNetwork.Services
 
         static Random Random = new Random();
 
-        static string[] Names = new string[10] {
+        static string[] Names = new string[standartCount]
+        {
             "Vincent Little",
             "Matthew Garrett",
             "Sonja Hodges",
@@ -106,7 +133,7 @@ namespace SocialNetwork.Services
             "Esther Palmer"
         };
 
-        static string[] Sentences = new string[10]
+        static string[] Sentences = new string[standartCount]
         {
             "makudonarudo",
             "guguru",
@@ -118,6 +145,20 @@ namespace SocialNetwork.Services
             "miruku",
             "baasu biiru",
             "sutaabakkusu"
+        };
+
+        static string[] Avatars = new string[standartCount]
+        {
+            "https://i.redd.it/ajixtzm7f6221.jpg",
+            "https://media-01.creema.net/user/2061109/exhibits/4349352/0_5869938eb999b1235fe3a7fe7ac8d72b_583x585.jpg",
+            "https://cdn4.vectorstock.com/i/1000x1000/58/03/cute-kitten-domestic-pet-pixel-art-isolated-vector-18645803.jpg",
+            "https://ih0.redbubble.net/image.432932179.2151/flat,550x550,075,f.u1.jpg",
+            "https://img21.shop-pro.jp/PA01377/893/product/120448091.jpg?cmsp_timestamp=20170719142403",
+            "https://dplhqivlpbfks.cloudfront.net/box_resize/1220x1240/6f2d5a8c-392305.jpg",
+            "https://dejpknyizje2n.cloudfront.net/marketplace/products/anime-pixel-art-girl-with-purple-and-pink-hair-sticker-1539282612.7939198.png",
+            "https://dejpknyizje2n.cloudfront.net/marketplace/products/8-bit-pixel-art-pizza-sticker-1542411683.8391273.png",
+            "https://www.sccpre.cat/mypng/detail/319-3194557_pixel-fox-easy-pixel-art-cat.png",
+            "https://images-na.ssl-images-amazon.com/images/I/11Xv6Hb3OyL._SY355_.png"
         };
     }
 }
