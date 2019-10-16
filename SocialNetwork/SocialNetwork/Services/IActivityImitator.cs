2d3e03e4984cf6ef0f3bdb1dcf3df81fbb9293af
Catherine <Catherine25@users.noreply.github.com>
2019-10-16 10:28:59 +0300

    Add IActivityImitator.cs

---
+++ SocialNetwork/SocialNetwork/Services/IActivityImitator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.Services
+{
+    interface IActivityImitator
+    {
+        //DateTime StartTime { get; set; }
+        //TimeSpan Frequency { get; set; }
+        //void TryWork(object o);
+        //void SuspendRequest();
+    }
+}
