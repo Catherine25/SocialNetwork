97f464e3c308836296f4ca229e7a5c01163d5b6b
Catherine <Catherine25@users.noreply.github.com>
2019-10-16 10:23:33 +0300

    Add ButtonExtensions.cs

---
+++ SocialNetwork/SocialNetwork/Services/ButtonExtensions.cs
@@ -0,0 +1,17 @@
+﻿using SocialNetwork.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SocialNetwork.Services
+{
+    public static class ButtonExtensions
+    {
+        public static void SetTheme(this Button button, Theme theme)
+        {
+            button.TextColor = theme.TextColor;
+            button.BackgroundColor = theme.BackgroundColor;
+        }
+    }
+}
