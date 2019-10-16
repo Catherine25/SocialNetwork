66126ae22e96934897e740c19b3b25223a11ed44
Catherine <Catherine25@users.noreply.github.com>
2019-10-16 10:29:13 +0300

    Add IColorable.cs

---
+++ SocialNetwork/SocialNetwork/Services/IColorable.cs
@@ -0,0 +1,12 @@
+﻿using SocialNetwork.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.Services
+{
+    interface IColorable
+    {
+        void SetTheme(Theme theme);
+    }
+}
