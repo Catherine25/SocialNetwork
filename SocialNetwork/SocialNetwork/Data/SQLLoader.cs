cb0289cedbe625ad0e3c1652b1091113afc8f630
Catherine25 <CatherineSolodoukhina@gmail.com>
2019-10-29 18:42:57 +0200

    Mone connectionString and items to class variables

--- SocialNetwork/SocialNetwork/Data/SQLLoader.cs
+++ SocialNetwork/SocialNetwork/Data/SQLLoader.cs
@@ -2,37 +2,31 @@
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using SocialNetwork.Data.Database;
 
 namespace SocialNetwork.Data
 {
 	class SQLLoader : ILoader
 	{
-        public SQLLoader()
+		string connectionString;
+		List<string> items = new List<string>();
+
+		public SQLLoader()
 		{
 			string server = "35.180.63.12";
 			string database = "Kate";
 			string uid = "Admin";
 			string password = "3555serg";
-			string connectionString =
+			connectionString =
                 "SERVER=" + server +
                 "; PORT = 3306 ;" +
                 "DATABASE=" + database + ";" +
                 "UID=" + uid + ";" +
                 "PASSWORD=" + password + ";";
-
-            LoadUsersFromDB(connectionString);
-        }
-
-        private void LoadUsersFromDB(string connectionString)
-        {
-            ConnectToDB(connectionString, SelectAllUsers, User_Username);
         }
 
         private void ConnectToDB(string connectionString, string sqlCommand, string field)
         {
-            List<string> items = new List<string>();
-
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             using (MySqlCommand cmd = new MySqlCommand(sqlCommand, connection))
             {
@@ -45,19 +39,23 @@ namespace SocialNetwork.Data
                         // Read advances to the next row.
                         while (reader.Read())
                         {
-                            // To avoid unexpected bugs access columns by name.
-                            items.Add(reader.GetString(reader.GetOrdinal(field)));
+							if(field == null || field == "")
+							{
+								string whatIsThis = reader.ToString();
+								//string name = reader.().ToString();
+							}
+							else
+							{
+								// To avoid unexpected bugs access columns by name.
+								items.Add(reader.GetString(reader.GetOrdinal(field)));
+							}
                         }
                     }
                 }
             }
-            foreach (var item in items)
-            {
-                Console.WriteLine(item);
-            }
         }
 
-		public IEnumerable<User> LoadFriends()
+		public List<User> LoadFriends()
 		{
 			throw new NotImplementedException();
 		}
@@ -67,14 +65,13 @@ namespace SocialNetwork.Data
 			throw new NotImplementedException();
 		}
 
-		public User LoadUser()
+		public User LoadUser(string name)
 		{
-			throw new NotImplementedException();
-		}
-
-		List<User> ILoader.LoadFriends()
-		{
-			throw new NotImplementedException();
+			ConnectToDB(connectionString,
+				new SQLCommands().SelectAllUsers,
+				"");
+			items.Find(X => X == name);
+			return new User(null, null, null);
 		}
     }
 }
