d9fb7eb36d10a4db4de35e0b6828febc1a78fc5f
Catherine <Catherine25@users.noreply.github.com>
2019-11-17 18:41:16 +0200

    Set _loader type to SQLLoader Remove UserChangeRequest Commented: - TryAddConversation() - BotGeneratedMessage()

--- SocialNetwork/SocialNetwork/App.xaml.cs
+++ SocialNetwork/SocialNetwork/App.xaml.cs
@@ -15,18 +15,22 @@ namespace SocialNetwork
     public partial class App : Application
     {
         #region Variables
+
         NewMessagesImitator _bot;
         Themes _themes;
         User _currentUser;
-		ILoader _loader;
+		SQLLoader _loader;
         LocalData _localData;
+        
         #endregion
 
         ///<summary> Constructor </summary>
         public App()
         {
             InitializeComponent();
+
             _localData = new LocalData();
+            _themes = new Themes();
 
             try
             {
@@ -35,64 +39,53 @@ namespace SocialNetwork
             }
             catch
             {
-                //Debug.WriteLine("Failed. Connecting to generator");
+                Debug.WriteLine("Failed. Connecting to generator");
                 //_loader = new GeneratorLoader();
             }
 			
-            _themes = new Themes();
             // _bot = new NewMessagesImitator(_currentUser, DateTime.Now, TimeSpan.FromSeconds(5));
             // _bot.MessageGenerated += BotGeneratedMessage;
 
-            MainPage = new MainPage(null, _themes, _localData);
-            (MainPage as MainPage).UserChangeRequest += UserChangeRequest;
+            MainPage = new MainPage(_themes, _localData, _loader);
         }
-
-		private void UserChangeRequest(User user)
-		{
-			_currentUser = user;
-			_localData.ChangeUser(user);
-			MainPage = new MainPage(_currentUser, _themes, _localData);
-			_currentUser.Friends = _localData.FindFriendsOfUser(_currentUser);
-			_currentUser.Groups = _localData.FindGroupsOfUser(_currentUser);
-		}
 
 		/// <summary>Special method with validation</summary>
-		private void TryAddConversation(Conversation conversation)
-        {
-            Debug.WriteLine("TryAddConversation() running");
+		//private void TryAddConversation(Conversation conversation)
+  //      {
+  //          Debug.WriteLine("TryAddConversation() running");
 
-            User user1 = conversation.member1;
-            User user2 = conversation.member2;
+  //          User user1 = conversation.member1;
+  //          User user2 = conversation.member2;
 
-            Debug.WriteLine("user1 = " + user1 + " user2 = " + user2);
+  //          Debug.WriteLine("user1 = " + user1 + " user2 = " + user2);
 
-            Conversation existing;
-            existing = _localData.Conversations.Find(X => X.member1.Name == user1.Name && X.member2.Name == user2.Name);
+  //          Conversation existing;
+  //          existing = _localData.Conversations.Find(X => X.member1.Name == user1.Name && X.member2.Name == user2.Name);
 
-            if (existing == null)
-            {
-                Debug.WriteLine("existing conversation not found");
-                existing = _localData.Conversations.Find(X => X.member1.Name == user2.Name && X.member2.Name == user1.Name);
-            }
+  //          if (existing == null)
+  //          {
+  //              Debug.WriteLine("existing conversation not found");
+  //              existing = _localData.Conversations.Find(X => X.member1.Name == user2.Name && X.member2.Name == user1.Name);
+  //          }
 
-            if (existing == null)
-            {
-                Debug.WriteLine("existing conversation not found again, adding");
-                _localData.Conversations.Add(conversation);
-            }
-            else
-            {
-                Debug.WriteLine("existing conversation_1 found");
-                existing.messages.AddRange(conversation.messages);
-            }
-        }
+  //          if (existing == null)
+  //          {
+  //              Debug.WriteLine("existing conversation not found again, adding");
+  //              _localData.Conversations.Add(conversation);
+  //          }
+  //          else
+  //          {
+  //              Debug.WriteLine("existing conversation_1 found");
+  //              existing.messages.AddRange(conversation.messages);
+  //          }
+  //      }
 
         ///<summary> Rises after NewMessagesImitator.MessageGenerated </summary>
-        private void BotGeneratedMessage(Message message, User author)
-        {
-            //message.IsFromMember1 = true;
-            //TryAddConversation(new Conversation(author, _currentUser), new List<Message> { message }));
-        }
+        //private void BotGeneratedMessage(Message message, User author)
+        //{
+        //    //message.IsFromMember1 = true;
+        //    //TryAddConversation(new Conversation(author, _currentUser), new List<Message> { message }));
+        //}
 
         #region Overridings
         protected override void OnStart()
