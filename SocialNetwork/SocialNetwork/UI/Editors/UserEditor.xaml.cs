3672e06c611a7358463376e0ba3aa4518f486010
Catherine <Catherine25@users.noreply.github.com>
2019-11-24 07:47:03 +0200

    Minor renamings

--- SocialNetwork/SocialNetwork/UI/Editors/UserEditor.xaml.cs
+++ SocialNetwork/SocialNetwork/UI/Editors/UserEditor.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using SocialNetwork.Data;
 using SocialNetwork.Data.Database;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
