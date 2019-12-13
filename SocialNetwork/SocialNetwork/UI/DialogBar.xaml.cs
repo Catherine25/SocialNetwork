dc209fd7f2266783e5e60763c72c008ef7a1af38
Catherine <Catherine25@users.noreply.github.com>
2019-12-14 00:14:27 +0200

    Add DialogBar

---
+++ SocialNetwork/SocialNetwork/UI/DialogBar.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SocialNetwork.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DialogBar : ContentView
+    {
+        public event Action ReturnRequest;
+
+        public DialogBar() => InitializeComponent();
+
+        public void Update(string link, string name)
+        {
+            _avatar.Source = ImageSource.FromUri(new Uri(link));
+            _name.Text = name;
+
+            _backButton.Clicked += (object o, EventArgs e) => ReturnRequest();
+        }
+    }
+}
