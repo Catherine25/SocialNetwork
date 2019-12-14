c34224d2a90e1ceebdb9349020c1a176118b9f10
Catherine <Catherine25@users.noreply.github.com>
2019-12-14 21:19:51 +0200

    Add: - NoUserAvatarLink - TrySetImage

--- SocialNetwork/SocialNetwork/UI/DialogBar.xaml.cs
+++ SocialNetwork/SocialNetwork/UI/DialogBar.xaml.cs
@@ -7,16 +7,29 @@ namespace SocialNetwork.UI
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DialogBar : ContentView
     {
+        private string NoUserAvatarLink = "https://www.indiannaturaloils.com/categories-images/no-photo.jpg";
         public event Action ReturnRequest;
 
         public DialogBar() => InitializeComponent();
 
         public void Update(string link, string name)
         {
-            _avatar.Source = ImageSource.FromUri(new Uri(link));
             _name.Text = name;
+            TrySetImage(link);
 
             _backButton.Clicked += (object o, EventArgs e) => ReturnRequest();
+        }
+
+        private void TrySetImage(string link)
+        {
+            try
+            {
+                _avatar.Source = ImageSource.FromUri(new Uri(link));
+            }
+            catch
+            {
+                _avatar.Source = NoUserAvatarLink;
+            }
         }
     }
 }
