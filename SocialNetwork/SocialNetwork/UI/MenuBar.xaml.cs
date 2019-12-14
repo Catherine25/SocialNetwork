7f6cc6b0a8ee57afa456eb1e7d69813fb2fa38ca
Catherine <Catherine25@users.noreply.github.com>
2019-12-14 09:57:34 +0200

    Make font larger Add: - OpenSettingsViewRequest - IncludeDebug()

--- SocialNetwork/SocialNetwork/UI/MenuBar.xaml.cs
+++ SocialNetwork/SocialNetwork/UI/MenuBar.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,17 +9,19 @@ namespace SocialNetwork.UI
     public partial class MenuBar : ContentView
     {
         public event Action<string> SearchRequest;
+        public event Action OpenSettingsViewRequest;
 
-        public MenuBar() => InitializeComponent();
+        public MenuBar()
+        {
+            InitializeComponent();
+            IncludeDebug();
+        }
 
         public void Update(string name)
         {
             _name.Text = name;
             
-            _menuButton.Clicked += (object o, EventArgs e) =>
-            {
-                throw new NotImplementedException();
-            };
+            _menuButton.Clicked += (object o, EventArgs e) => OpenSettingsViewRequest();
 
             _searchButton.Clicked += (object o, EventArgs e) =>
             {
@@ -33,7 +36,15 @@ namespace SocialNetwork.UI
                 _searchButton.IsVisible = true;
                 _companionNameEntry.IsVisible = false;
                 SearchRequest(_companionNameEntry.Text);
+                _companionNameEntry.Text = "";
             };
+        }
+
+        private void IncludeDebug()
+        {
+            _menuButton.Clicked += (object o, EventArgs e) => Debug.WriteLine("[m] [MenuBar] _menuButton Clicked");
+            _searchButton.Clicked += (object o, EventArgs e) => Debug.WriteLine("[m] [MenuBar] _searchButton Clicked");
+            _companionNameEntry.TextChanged += (object o, TextChangedEventArgs e) => Debug.WriteLine("[m] [MenuBar] _companionNameEntry Clicked");
         }
     }
 }
