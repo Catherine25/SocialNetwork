314e7fad2eb0883574427fcc49f5178c54b3e1d0
Catherine <Catherine25@users.noreply.github.com>
2019-12-14 09:49:55 +0200

    Remove: - _noFriendsLabel - _newFriendBt - NewFriendBt_Clicked() - SetTheme()

--- SocialNetwork/SocialNetwork/UI/Views/FriendsView.xaml.cs
+++ SocialNetwork/SocialNetwork/UI/Views/FriendsView.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using SocialNetwork.Data;
 using SocialNetwork.Data.Database;
-using SocialNetwork.Services;
 using SocialNetwork.UI.DataRequests;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,72 +12,45 @@ using Xamarin.Forms.Xaml;
 namespace SocialNetwork.UI.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FriendsView : ContentView, IColorable
+    public partial class FriendsView : ContentView
     {
-        public enum Mode { Editable, ChooseNew, ReadOnly }
-        private Mode _mode;
         private LocalData _localData;
         private User _user;
         
-        public List<User> Friends;
-        public List<string> FriendNames;
+        public List<User> Users;
+        public List<string> UserNames;
 
         public event Action<User> OpenUserViewRequest;
         public event Action<User, Conversation> SetNewConversationRequest;
 		public event Action<UserRequestDialog.RequestPurpose> ShowDialogRequest;
 
-        public FriendsView(User user, Mode mode, LocalData localData)
+        public FriendsView(User user, LocalData localData)
         {
             Debug.WriteLine("[m] [FriendsView] Constructor running");
 
             InitializeComponent();
 
-            Update(user, mode, localData);
+            Update(user, localData);
 
-			_newFriendBt.Clicked += NewFriendBt_Clicked;
             _listView.ItemSelected += ItemSelected;
         }
 
-        public void Update(User user, Mode mode, LocalData localData)
+        public void Update(User user, LocalData localData)
         {
             Debug.WriteLine("[m] [FriendsView] Update running");
 
-            _mode = mode;
             _localData = localData;
             _user = user;
 
-            _newFriendBt.IsVisible = mode != Mode.ReadOnly;
+            Users = _localData.GetUsers();
 
-            if (mode == Mode.ReadOnly)
-                _user.Friends = _localData.FindFriendsOfUser(_user);
+            UserNames = Users.Select(x => x.Name).ToList();
+            _listView.ItemsSource = UserNames;
 
-            if (_user.Friends.Count == 0)
-            {
-                _noFriendsLabel.IsVisible = true;
-                _listView.IsVisible = false;
-            }
-            else
-            {
-                _noFriendsLabel.IsVisible = false;
-                _listView.IsVisible = true;
-
-                Friends = user.Friends;
-                FriendNames = Friends.Select(x => x.Name).ToList();
-                _listView.ItemsSource = FriendNames;
-
-                BindingContext = this;
-            }
+            BindingContext = this;
         }
 
-
-        private void NewFriendBt_Clicked(object sender, EventArgs e)
-        {
-            Debug.WriteLine("[m] [FriendsView] NewFriendBt_Clicked running");
-
-            ShowDialogRequest(UserRequestDialog.RequestPurpose.newFriendName);
-        }
-
-        private void ItemSelected(object sender, SelectedItemChangedEventArgs e) 
+        private void ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             Debug.WriteLine("[m] [FriendsView] ItemSelected running");
 
@@ -88,25 +58,13 @@ namespace SocialNetwork.UI.Views
                 return;
 
             string friendName = e.SelectedItem as string;
-            User friend = Friends.Find(X => X.Name == friendName);
+            User friend = Users.Find(X => X.Name == friendName);
 
-            if (_mode == Mode.ChooseNew)
-            {
-                Conversation c = new Conversation(0, _user, friend);
-                _localData.AddEmptyConversation(c);
-                SetNewConversationRequest(friend, c);
-            }
-            else
-                OpenUserViewRequest(friend);
+            Conversation c = new Conversation(0, _user, friend);
+            _localData.AddEmptyConversation(c);
+            SetNewConversationRequest(friend, c);
 
             (sender as ListView).SelectedItem = null;
-        }
-
-        public void SetTheme(Theme theme)
-        {
-            Debug.WriteLine("[m] [FriendsView] SetTheme running");
-
-            (this as View).SetTheme(theme);
         }
     }
 }
